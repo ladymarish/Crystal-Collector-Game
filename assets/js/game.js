73d2afa978c103d3031418d4751a07244d925c3a
Marina-Saladukha <marina@reunionmarketing.com>
2018-02-10 23:28:52 -0500

    final

---
+++ assets/js/game.js
@@ -0,0 +1,64 @@
+//each crystal has a random number assigned
+//a random number is generated 
+//when you click on the crystal it adds a number to the box
+//if the number guessed is more then the number in the box - you lose
+
+$(document).ready(function() {
+
+var userGuess;
+var computerGuess;
+var crystalNumber;
+var crystalRandom;
+var wins;
+var losses;
+var totalScore;
+var gameEnd;
+var wins = 0;
+var losses = 0;
+
+//initiate the game
+function startGame () {
+   gameEnd = false;
+   userGuess = "";
+   totalScore = 0;
+   computerGuess = Math.floor((Math.random() * 100) + 1);
+   crystalRandom = Math.floor((Math.random() * 10) + 1);
+   imageCrystal = $(".cr");
+   $("#number").html(computerGuess);
+   $("#wins").html("Wins: " + wins);
+   $("#losses").html("Losses: " + losses);
+};
+
+//targets every class 
+$(".cr").each(function(index, item) {
+   crystalRandom = Math.floor((Math.random() * 10) + 1);
+   $(item).attr("crystals", crystalRandom);
+   var crystalValue = $(item).attr("crystals");
+   console.log(crystalValue);
+});
+   
+
+$(".cr").on("click", function() {
+   var crystalValue = $(this).attr("crystals");
+   totalScore = parseInt(totalScore) + parseInt(crystalValue);
+   $(".score").html(totalScore);
+   if (totalScore === computerGuess) {
+	  wins += 1;
+	  $("#wins").html("Wins: " + wins);
+	  gameEnd = true;
+	  startGame ();
+   }
+   else if (totalScore > computerGuess) {
+      losses += 1;
+      $("#losses").html("Losses: " + losses);
+      gameEnd = true;
+      startGame ();
+   }
+});
+
+
+
+startGame ();
+
+
+});
